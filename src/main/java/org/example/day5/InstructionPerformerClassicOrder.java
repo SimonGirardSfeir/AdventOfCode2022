6c8a47110aaaba9883e30ada01a2fe38cc418be4
Simon Girard <simon.girard1991@gmail.com>
2022-12-05 15:01:26 +0100

    remove List and replace by Deque

--- src/main/java/org/example/day5/InstructionPerformerClassicOrder.java
+++ src/main/java/org/example/day5/InstructionPerformerClassicOrder.java
@@ -1,24 +1,23 @@
 package org.example.day5;
 
-import java.util.ArrayList;
+import java.util.ArrayDeque;
 import java.util.Deque;
-import java.util.List;
 
 public class InstructionPerformerClassicOrder implements InstructionPerformer {
     @Override
     public StackCratesInventory applyInstruction(StackCratesInventory stackCratesInventory, Instruction instruction) {
         Deque<Character> sourceStack = stackCratesInventory.stackCrates().get(instruction.sourceStack()-1);
         Deque<Character> targetStack = stackCratesInventory.stackCrates().get(instruction.targetStack()-1);
-        List<Character> charToAdd = new ArrayList<>();
+        Deque<Character> charToAdd = new ArrayDeque<>();
         int index = instruction.numberOfElementToMove();
         while(index > 0) {
             Character character = sourceStack.pop();
-            charToAdd.add(character);
+            charToAdd.push(character);
             index--;
         }
 
-        for(int i = charToAdd.size() - 1; i >= 0; i--) {
-            targetStack.push(charToAdd.get(i));
+        while(!charToAdd.isEmpty()) {
+            targetStack.push(charToAdd.pop());
         }
         return stackCratesInventory;
     }
