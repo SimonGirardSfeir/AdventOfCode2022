3cada97ba063f72834a1972dcdd9b42dfeeaacdf
Simon Girard <simon.girard1991@gmail.com>
2022-12-07 00:54:33 +0100

    Day 6 improved from O(n*m) to O(n) in time performance

--- src/main/java/org/example/day6/MarkerResolver.java
+++ src/main/java/org/example/day6/MarkerResolver.java
@@ -1,6 +1,8 @@
 package org.example.day6;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class MarkerResolver {
@@ -9,17 +11,28 @@ public class MarkerResolver {
 
     public static int getFirstMarker(String datastreamBuffer, int markerLength) {
         Set<Character> tempSet = new HashSet<>();
-        for(int i = 0; i <datastreamBuffer.length() - markerLength; i++) {
-            for(int j = i; j < i+markerLength; j++) {
-                tempSet.add(datastreamBuffer.charAt(j));
-            }
-            if(tempSet.size() == markerLength) {
-                return i+markerLength;
-            } else {
-                tempSet = new HashSet<>();
-            }
+        List<Character> list = new ArrayList<>();
+        for(int i = 0; i <datastreamBuffer.length(); i++) {
+            Character currentCharacter = datastreamBuffer.charAt(i);
+            list.add(currentCharacter);
+            tempSet.add(currentCharacter);
+
+            if(i >= markerLength)
+                removeNonRelevantCharactersForMarker(markerLength, tempSet, list, i);
+
+            if(tempSet.size() == markerLength)
+                return i+1;
 
         }
         return 0;
+    }
+
+    private static void removeNonRelevantCharactersForMarker(int markerLength, Set<Character> tempSet, List<Character> list, int i) {
+        Character characterToRemove = list.get(i - markerLength);
+        //Set is chosen because contains has better time complexity, O(1) vs O(n)
+        Set<Character> subListSet = new HashSet<>(list.subList(i - markerLength + 1, i +1));
+        if(!subListSet.contains(characterToRemove)) {
+            tempSet.remove(characterToRemove);
+        }
     }
 }
