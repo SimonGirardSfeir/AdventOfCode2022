67d2ed7fd9724ee28b2944137144462211f6963a
Simon Girard <simon.girard1991@gmail.com>
2022-12-10 00:21:46 +0100

    refacto

--- src/main/java/org/example/day9/Position.java
+++ src/main/java/org/example/day9/Position.java
@@ -18,6 +18,7 @@ public final class Position {
         this.x = x;
         this.y = y;
     }
+
     public static Position of(int ropeSize) {
         Position position = new Position(0, 0);
         Position currentPosition = position;
@@ -54,36 +55,6 @@ public final class Position {
         }
         applyDirectionToPreviousPosition();
     }
-    private boolean isCurrentPositionTouchingPrevious() {
-        return !(Math.abs(previous.getX() - x) < 2 && (Math.abs(previous.getY() - y)) < 2);
-    }
-
-    private void applyDirectionToPreviousPosition() {
-        if (isPreviousPositionCandidateToApplyDirection()) {
-            if (y > previous.y && x < previous.x)
-                previous.applyDirection(UPLEFT);
-            else if (y > previous.y && x > previous.x)
-                previous.applyDirection(UPRIGHT);
-            else if (y < previous.y && x < previous.x)
-                previous.applyDirection(DOWNLEFT);
-            else if (y < previous.y && x > previous.x)
-                previous.applyDirection(DOWNRIGHT);
-            else if (y == previous.y && x < previous.x)
-                previous.applyDirection(LEFT);
-            else if (y == previous.y && x > previous.x)
-                previous.applyDirection(RIGHT);
-            else if (y > previous.y && x == previous.x)
-                previous.applyDirection(UP);
-            else if (y < previous.y && x == previous.x)
-                previous.applyDirection(DOWN);
-
-        }
-    }
-
-    private boolean isPreviousPositionCandidateToApplyDirection() {
-        return previous != null && isCurrentPositionTouchingPrevious();
-    }
-
     public Position getLastPosition() {
         Position currentPosition = this;
         while(currentPosition.previous != null) {
@@ -91,16 +62,50 @@ public final class Position {
         }
         return currentPosition;
     }
+    private void applyDirectionToPreviousPosition() {
+        if (isPreviousPositionCandidateToApplyDirection()) {
+            if (isNextDirectionUpLeft())
+                previous.applyDirection(UPLEFT);
+            else if (isNextDirectionUpRight())
+                previous.applyDirection(UPRIGHT);
+            else if (isNextDirectionDownLeft())
+                previous.applyDirection(DOWNLEFT);
+            else if (isNextDirectionDownRight())
+                previous.applyDirection(DOWNRIGHT);
+            else if (y == previous.y && x < previous.x)
+                previous.applyDirection(LEFT);
+            else if (y == previous.y && x > previous.x)
+                previous.applyDirection(RIGHT);
+            else if (y > previous.y)
+                previous.applyDirection(UP);
+            else if (y < previous.y)
+                previous.applyDirection(DOWN);
+
+        }
+    }
+    private boolean isCurrentPositionTouchingPrevious() {
+        return !(Math.abs(previous.x - x) < 2 && (Math.abs(previous.y - y)) < 2);
+    }
+    private boolean isNextDirectionDownRight() {
+        return y < previous.y && x > previous.x;
+    }
+    private boolean isNextDirectionDownLeft() {
+        return y < previous.y && x < previous.x;
+    }
+    private boolean isNextDirectionUpRight() {
+        return y > previous.y && x > previous.x;
+    }
+    private boolean isNextDirectionUpLeft() {
+        return y > previous.y && x < previous.x;
+    }
+    private boolean isPreviousPositionCandidateToApplyDirection() {
+        return previous != null && isCurrentPositionTouchingPrevious();
+    }
 
     private void setPrevious(Position position) {
         this.previous = position;
     }
-    public int getX() {
-        return x;
-    }
-    public int getY() {
-        return y;
-    }
+
     @Override
     public String toString() {
         return  x + " " + y;
